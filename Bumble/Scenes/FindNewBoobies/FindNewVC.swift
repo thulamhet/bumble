0cd281989198edafad0286163f9e1fe081a8db9e
thu <congthu137@gmail.com>
2023-07-06 17:48:39 +0700

    some fixes

---
+++ Bumble/Scenes/FindNewBoobies/FindNewVC.swift
@@ -0,0 +1,21 @@
+//
+//  FindNewVC.swift
+//  Bumble
+//
+//  Created by Nguyễn Công Thư on 06/07/2023.
+//
+
+import UIKit
+
+class FindNewVC: UIViewController {
+    @IBOutlet weak var scrollView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView () {
+        
+    }
+}
---
+++ Bumble/Scenes/FindNewBoobies/SwipeProfileVC.swift
@@ -0,0 +1,16 @@
+//
+//  SwipeProfileVC.swift
+//  Bumble
+//
+//  Created by Nguyễn Công Thư on 06/07/2023.
+//
+
+import UIKit
+
+class SwipeProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
